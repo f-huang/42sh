b62fa4a80bf28d33a15833cfc8bad682d90c16b9
Yoann FUKS <yfuks@e3r3p9.42.fr>
2016-11-08 23:11:36 +0100

    Add env keys utilitys

---
+++ src/builtins/env/update_key.c
@@ -0,0 +1,29 @@
+#include "builtin_env.h"
+#include "libft.h"
+#include <stdlib.h>
+
+static	int	found_line_with_key(char *key, char **env)
+{
+	int		i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(key, env[i], ft_strlen(key)) == 0)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int			env_update_key(char *key, char *str, char **env)
+{
+	int		line;
+	char	*tmp;
+
+	line = found_line_with_key(key, env);
+	tmp = env[line];
+	env[line] = ft_strdup(str);
+	free(tmp);
+	return (0);
+}
