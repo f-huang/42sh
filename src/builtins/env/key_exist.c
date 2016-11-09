b998a87d2db9a49f932979da1c65154ea6cb2b63
Yoann FUKS <yfuks@e3r3p9.42.fr>
2016-11-09 12:16:59 +0100

    fix key_exist

--- src/builtins/env/key_exist.c
+++ src/builtins/env/key_exist.c
@@ -4,13 +4,15 @@
 int		env_key_exist(char *key, char **env)
 {
 	int		i;
+	int		key_len;
 
 	if (env == 0)
 		return (0);
 	i = 0;
+	key_len = ft_strlen(key);
 	while (env[i])
 	{
-		if (ft_strncmp(key, env[i], ft_strlen(key)) == 0)
+		if (ft_strncmp(key, env[i], key_len) == 0 && env[i][key_len] == '=')
 			return (1);
 		i++;
 	}
