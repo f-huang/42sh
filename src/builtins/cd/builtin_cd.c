c87b1ccf02899109dcbb381d2c0b576743fc6920
kalak-io <cjacquem@student.42.fr>
2016-11-09 15:51:19 +0100

    MAJ for compilation of builtin_cd

--- src/builtins/cd/builtin_cd.c
+++ src/builtins/cd/builtin_cd.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
 #include <limits.h>
+#include "ft_42sh.h"
 #include "builtin_cd.h"
 #include "environment.h"
+#include "libft.h"
 
 /*
 **			This function change the working directory of the current shell
@@ -13,16 +15,16 @@ static void	set_pwd(t_environment *lst_env)
 	char	*tmp;
 
 	if ((tmp = sh_getenv(lst_env, "PWD")))
-		sh_setenv(lst_env, "OLDPWD", tmp);
+		sh_setenv(&lst_env, "OLDPWD", tmp);
 	tmp = NULL;
 	if ((tmp = getcwd(tmp, _POSIX_PATH_MAX)))
 	{
-		sh_setenv(lst_env, "PWD", tmp);
+		sh_setenv(&lst_env, "PWD", tmp);
 		ft_strdel(&tmp);
 	}
 }
 
-static void	change_directory(char *path)
+static void	change_directory(t_environment *lst_env, char *path)
 {
 	if (chdir(path) == -1)
 	{
@@ -39,7 +41,7 @@ static void	go_to(t_environment *lst_env, int dest)
 	char	*p;
 
 	if ((p = sh_getenv(lst_env, dest ? "HOME" : "OLDPWD")))
-		change_directory(p);
+		change_directory(lst_env, p);
 	else
 	{
 		ft_putstr_fd("cd: ", 2);
@@ -51,14 +53,14 @@ static void	go_to(t_environment *lst_env, int dest)
 int			builtin_cd(t_shell *sh, int ac, char **av)
 {
 	if (ac == 1)
-		go_to(lst_env, HOME);
+		go_to(sh->lst_env, HOME);
 	else if (ac == 2)
 	{
 		if (av[1][0] == '/' || ft_strnequ(av[1], "./", 2)\
 							|| ft_strnequ(av[1], "../", 3))
-			change_directory(av[1]);
+			change_directory(sh->lst_env, av[1]);
 		else if (ft_strequ(av[1], "-"))
-			go_to(lst_env, OLDPWD);
+			go_to(sh->lst_env, OLDPWD);
 	}
 	else if (ac == 3)
 	{
@@ -66,4 +68,5 @@ int			builtin_cd(t_shell *sh, int ac, char **av)
 	}
 	else
 		ft_putstr_fd("cd: Too many arguments", 2);
+	return (GOOD);
 }
