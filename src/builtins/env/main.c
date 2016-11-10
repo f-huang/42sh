6c998d74ac3ad9e031eb0075671851e7342efacf
Yoann FUKS <yfuks@e3r4p10.42.fr>
2016-11-10 15:39:55 +0100

    Fix leaks env builtin

--- src/builtins/env/main.c
+++ src/builtins/env/main.c
@@ -6,10 +6,11 @@
 /*   By: yfuks <yfuks@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 15:44:26 by yfuks             #+#    #+#             */
-/*   Updated: 2016/11/09 16:59:15 by yfuks            ###   ########.fr       */
+/*   Updated: 2016/11/10 15:39:09 by yfuks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "builtin_env.h"
 #include "libft.h"
 
@@ -17,9 +18,12 @@ int			main(int ac, char **av, char **env)
 {
 	char	**internal_env;
 	int		i;
+	int		status;
 
 	if (ac < 2)
 		return (env_print_env(env));
 	internal_env = env_copy_env(env);
-	return (env_parse_argv(i, ac, av, &internal_env));
+	status = env_parse_argv(i, ac, av, &internal_env);
+	env_free_env(&internal_env);
+	return (status);
 }
