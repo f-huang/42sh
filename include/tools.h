1d955a9cf5e267bf791c536fd563c4fb00873d62
Yoann FUKS <yfuks@e3r5p8.42.fr>
2016-11-11 16:15:48 +0100

    Fix compilation

--- include/tools.h
+++ include/tools.h
@@ -6,12 +6,14 @@
 /*   By: cjacquem <cjacquem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 18:12:33 by cjacquem          #+#    #+#             */
-/*   Updated: 2016/11/10 17:51:26 by fhuang           ###   ########.fr       */
+/*   Updated: 2016/11/11 16:15:22 by yfuks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOOLS_H
 # define TOOLS_H
+
+# include <unistd.h>
 
 int			tl_isspace(int c);
 int			tl_atoi_base(const char *str, int base);
--- src/init_shell.c
+++ src/init_shell.c
@@ -1,4 +1,5 @@
 #include <limits.h>
+#include <unistd.h>
 #include "ft_42sh.h"
 #include "tools.h"
 #include "environment.h"
