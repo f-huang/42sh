a1ae8945666c6c2f264e4baaf30774d1b9b6cf9f
Yoann FUKS <yfuks@e3r7p19.42.fr>
2016-11-21 21:08:44 +0100

    minor

--- src/execution/exec_closes_and_redirects.c
+++ src/execution/exec_closes_and_redirects.c
@@ -6,19 +6,19 @@
 /*   By: yfuks <yfuks@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 17:38:44 by yfuks             #+#    #+#             */
-/*   Updated: 2016/11/17 18:08:39 by yfuks            ###   ########.fr       */
+/*   Updated: 2016/11/21 15:37:49 by yfuks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include "execution.h"
 
 static	void	left_redirect(t_redirections *cursor)
 {
-// <&- + 1<&-
+	int		fd;
+    // <&- + 1<&-
 	if (cursor->type & CLOSE_REDIRECT)
 	{
 		cursor->from_fd = (cursor->from_fd == -1) ? 0 : cursor->from_fd;
@@ -31,7 +31,8 @@ static	void	left_redirect(t_redirections *cursor)
 		if (!exec_is_readable(cursor->dest))
 			exit(exec_print_command_error(PERMISSIONDENIED, cursor->dest));
 		cursor->from_fd = (cursor->from_fd == -1) ? 0 : cursor->from_fd;
-		dup2(open(cursor->dest, O_RDONLY, S_IRUSR | S_IWUSR), cursor->from_fd);
+		fd = open(cursor->dest, O_RDONLY, S_IRUSR | S_IWUSR);
+		dup2(fd, cursor->from_fd);
 	}
 	else // <&2 + 1<&2
 	{
@@ -39,11 +40,13 @@ static	void	left_redirect(t_redirections *cursor)
 			exit(exec_print_command_error(BADFILEDESCRIPTOR, ft_itoa(cursor->to_fd)));
 		cursor->from_fd = (cursor->from_fd == -1) ? 0 : cursor->from_fd;
 		dup2(cursor->to_fd, cursor->from_fd);
+		close(cursor->to_fd);
 	}
 }
 
 static	void	right_redirect(t_redirections *cursor)
 {
+	int		fd;
 	// >&- + 1>&-
 	if (cursor->type & CLOSE_REDIRECT)
 	{
@@ -57,7 +60,8 @@ static	void	right_redirect(t_redirections *cursor)
 		if (exec_is_file(cursor->dest) && !exec_is_writable(cursor->dest))
 			exit(exec_print_command_error(PERMISSIONDENIED, cursor->dest));
 		cursor->from_fd = (cursor->from_fd == -1) ? 1 : cursor->from_fd;
-		dup2(open(cursor->dest, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR), cursor->from_fd);
+		fd = open(cursor->dest, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+		dup2(fd, cursor->from_fd);
 	}
 	else // >&2 + 1>&2
 	{
@@ -65,6 +69,23 @@ static	void	right_redirect(t_redirections *cursor)
 			exit(exec_print_command_error(BADFILEDESCRIPTOR, ft_itoa(cursor->to_fd)));
 		cursor->from_fd = (cursor->from_fd == -1) ? 1 : cursor->from_fd;
 		dup2(cursor->to_fd, cursor->from_fd);
+		close(cursor->to_fd);
+	}
+}
+
+static	void	double_right_redirect(t_redirections *cursor)
+{
+	int		fd;
+
+	if (cursor->type & FILE_REDIRECT) // >> toto + 1>> toto
+	{
+		if (exec_is_file(cursor->dest) && exec_is_directory(cursor->dest))
+			exit(exec_print_command_error(ISDIRECTORY, cursor->dest));
+		if (exec_is_file(cursor->dest) && !exec_is_writable(cursor->dest))
+			exit(exec_print_command_error(PERMISSIONDENIED, cursor->dest));
+		cursor->from_fd = (cursor->from_fd == -1) ? 1 : cursor->from_fd;
+		fd = open(cursor->dest, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+		dup2(fd, cursor->from_fd);
 	}
 }
 
@@ -79,6 +100,8 @@ void	close_and_redirects(t_redirections **list)
 			left_redirect(cursor);
 		else if (cursor->type & SIMPLE_RIGHT_REDIRECT)
 			right_redirect(cursor);
+		else if (cursor->type & DOUBLE_RIGHT_REDIRECT)
+			double_right_redirect(cursor);
 		cursor = cursor->next;
 	}
 }
--- src/get_redirection.c
+++ src/get_redirection.c
@@ -6,7 +6,7 @@
 /*   By: yfuks <yfuks@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 03:33:41 by yfuks             #+#    #+#             */
-/*   Updated: 2016/11/17 19:29:51 by yfuks            ###   ########.fr       */
+/*   Updated: 2016/11/21 14:07:01 by yfuks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,12 +101,56 @@ static t_cmdwr	*get_exemple1(int print_info)
 	return (r);
 }
 
+static t_cmdwr	*get_exemple3(int print_info)
+{
+	t_cmdwr		*r;
+
+	if (!(r = (t_cmdwr*)malloc(sizeof(t_cmdwr))))
+		return (0);
+	if (print_info)
+	{
+		ft_putendl("Creating command:");
+		ft_putendl("cat <&- 1<&- auteur");
+	}
+	// cat <&- 1<&- auteur
+	r->command = (char **)malloc(sizeof(char *) * 4);
+	r->command[0] = ft_strdup("cat");
+	r->command[1] = ft_strdup("auteur");
+	r->command[2] = 0;
+	r->command[3] = 0;
+	r->redirs = (t_redirections*)malloc(sizeof(t_redirections));
+
+	// <&-
+	r->redirs->type = SIMPLE_LEFT_REDIRECT | CLOSE_REDIRECT;
+	r->redirs->from_fd = -1;
+	r->redirs->to_fd = -1;
+	r->redirs->dest = 0;
+
+	// 1<&-
+	r->redirs->next = (t_redirections*)malloc(sizeof(t_redirections));
+	r->redirs->next->type = CLOSE_REDIRECT | SIMPLE_LEFT_REDIRECT;
+	r->redirs->next->from_fd = 1;
+	r->redirs->next->to_fd = -1;
+	r->redirs->next->dest = 0;
+	r->redirs->next->next = 0;
+
+	// 2>> errors
+/*	r->redirs->next->next = (t_redirections*)malloc(sizeof(t_redirections));
+	r->redirs->next->next->type = DOUBLE_RIGHT_REDIRECT | FILE_REDIRECT;
+	r->redirs->next->next->from_fd = 2;
+	r->redirs->next->next->to_fd = -1;
+	r->redirs->next->next->dest = ft_strdup("errors");
+	r->redirs->next->next->next = 0;*/
+	return (r);
+}
+
 t_cmdwr		*get_redirection(char **command)
 {
 	t_cmdwr		*r;
 
 	(void)command;
-	r = get_exemple2(1);
+	r = get_exemple3(1);
+	get_exemple2(0);
 	get_exemple1(0);
 	return (r);
 }
