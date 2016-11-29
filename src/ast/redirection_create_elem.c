69dbe5e62b8a69c28a56203aa2b99cdc7ee461cb
Fanny Huang <fhuang@student.42.fr>
2016-11-29 21:43:58 +0100

    Fix double left redirect

--- src/ast/redirection_create_elem.c
+++ src/ast/redirection_create_elem.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 18:32:18 by fhuang            #+#    #+#             */
-/*   Updated: 2016/11/29 13:34:09 by fhuang           ###   ########.fr       */
+/*   Updated: 2016/11/29 21:43:25 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,10 @@ static void		get_redir_type(t_redirections **new, char *str)
 				else if (j == 2)
 					(*new)->type |= SIMPLE_RIGHT_REDIRECT;
 				else if (j == 1)
-					(*new)->type |= DOUBLE_LEFT_REDIRECT;
-				else if (j == 0)
 					(*new)->type |= DOUBLE_RIGHT_REDIRECT;
+				else if (j == 0)
+					(*new)->type |= DOUBLE_LEFT_REDIRECT;
+				break ;
 			}
 	}
 }
@@ -80,8 +81,8 @@ static int
 	i = 0;
 	if (ft_isdigit(str[i]))
 		(*new)->from_fd = ft_atoi(str + i++);
-	if ((*new)->type & DOUBLE_LEFT_REDIRECT)
-		return (GOOD); // DOUBLE_LEFT_REDIRECT FUNCITON
+	if ((*new)->type & DOUBLE_LEFT_REDIRECT) //DOUBLE LEFT
+		return (redirection_push_back(redir, *new));
 	if ((*new)->type & DOUBLE_RIGHT_REDIRECT)
 		i++;
 	if (str[++i] == '&')
