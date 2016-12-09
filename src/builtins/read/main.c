dc9dbffb0b4b4ff4321ba3dcb32aed63940dff49
Fanny HUANG <fhuang@e3r5p19.42.fr>
2016-12-09 15:47:07 +0100

    init branch

--- src/builtins/read/main.c
+++ src/builtins/read/main.c
@@ -6,21 +6,30 @@
 /*   By: cjacquem <cjacquem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 11:51:06 by cjacquem          #+#    #+#             */
-/*   Updated: 2016/12/08 15:56:19 by fhuang           ###   ########.fr       */
+/*   Updated: 2016/12/09 15:47:01 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 **		Synopsis
-**		read [-ers] [-u <FD>] [-t <TIMEOUT>] [-p <PROMPT>] [-a <ARRAY>]\
+**		read [-ers] [-u <FD>] [-t <TIMEOUT>] [-p <PROMPT>]
 **		[-n <NCHARS>] [-d <DELIM>] [-i <TEXT>] [<NAME...>]
+**
+**		-d : Delim = 1 char
+**		-e : Write output in the terminal (Readline() ?)
+**		-n : ?
+**		-p : ?
+**		-r : backslash(\) do not escape characters
+**		-s : Dont echo input on terminal
+
 */
 
 #include "ft_42sh.h"
 
+#define NB_OPTIONS 8
+
 int		main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	int		option[NB_OPTIONS];
 	return (0);
 }
