ad1291ce6d20624e823b1a1e60c7a41d3bc6e25b
Yoann FUKS <yfuks@e3r3p10.42.fr>
2017-01-14 18:18:31 +0100

    Fix exit with redirection

--- src/execution/exec_redirection.c
+++ src/execution/exec_redirection.c
@@ -6,7 +6,7 @@
 /*   By: yfuks <yfuks@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 02:07:16 by yfuks             #+#    #+#             */
-/*   Updated: 2016/12/13 16:21:28 by yfuks            ###   ########.fr       */
+/*   Updated: 2017/01/14 18:16:07 by yfuks            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int		exec_redirection(t_shell *sh, t_cmdwr *r)
 		waitpid(0, &tmp, WUNTRACED | WCONTINUED);
 		sh->last_return = get_command_status_code(tmp);
 		free_heredocs(r);
+		if (ft_strcmp(r->command[0], "exit") == 0)
+			exit(sh->last_return);
 		return (sh->last_return);
 	}
 	else if (id == 0)
