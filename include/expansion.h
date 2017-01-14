4d9af33a0612533e874f7a4bdc43f0dfc11e21b9
Adam TAGUIROV <ataguiro@e2r2p7.42.fr>
2017-01-14 13:35:38 +0100

    update, to norme

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 18:23:46 by fhuang            #+#    #+#             */
-/*   Updated: 2017/01/14 13:09:36 by ataguiro         ###   ########.fr       */
+/*   Updated: 2017/01/14 13:34:20 by ataguiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,10 @@
 # include "ft_42sh.h"
 # include "execution.h"
 
-typedef struct	s_jobs
-{
-	pid_t			pgid;
-	struct termios	tmodes;
-	int				sterm;
-	int				interact;
-}				t_jobs;
-
-extern t_jobs	g_jobs;
-
 char					*substitute(t_shell *sh, char *cmd);
 char					*dollar(t_shell *sh, char *acmd, char *ptr);
 int						tilde(char **acmd);
 char					*remove_quotes_and_backslash(char *cmd);
-void					jobs_init(void);
 
 int						alias_substitution(t_variable *lst_alias, char **line);
 #endif
--- src/init_shell.c
+++ src/init_shell.c
@@ -6,7 +6,7 @@
 /*   By: yfuks <yfuks@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 14:19:21 by yfuks             #+#    #+#             */
-/*   Updated: 2017/01/14 13:25:56 by ataguiro         ###   ########.fr       */
+/*   Updated: 2017/01/14 13:34:51 by ataguiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,6 @@ static int	set_default(t_variable **lst_env)
 
 int			init_shell(t_shell *sh, char *av_0)
 {
-	jobs_init();
 	ft_bzero(sh, sizeof(t_shell));
 	if (!copy_environment(&sh->lst_env))
 		return (ERROR);
