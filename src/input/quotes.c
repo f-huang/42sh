2608eebdbc939f4d69676848be11546640971bc9
Adam TAGUIROV <ataguiro@e3r9p15.42.fr>
2017-01-18 18:50:57 +0100

    to be continued

--- src/input/quotes.c
+++ src/input/quotes.c
@@ -6,13 +6,25 @@
 /*   By: ataguiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 12:49:07 by ataguiro          #+#    #+#             */
-/*   Updated: 2017/01/14 15:51:34 by ataguiro         ###   ########.fr       */
+/*   Updated: 2017/01/18 18:47:10 by ataguiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "input.h"
+/*
+static void	manage_bs(void)
+{
+	int		bs;
+	int		i;
 
-void	manage_quotes(void)
+	bs = 0;
+	i = 0;
+	while (!(*stock()) && (*command())[i])
+	{
+		
+	}
+}*/
+void		manage_quotes(void)
 {
 	int		i;
 	int		dq;
@@ -21,9 +33,10 @@ void	manage_quotes(void)
 	i = 0;
 	q = 0;
 	dq = 0;
+//	manage_bs();
 	while (!(*stock()) && (*command())[i])
 	{
-		if ((*command())[i] == '"' && !(q % 2))
+		if ((*command())[i] == '"' && !(q % 2) && !(*bs()))
 			dq++;
 		if ((*command())[i] == '\'' && !(dq % 2))
 			q++;
@@ -31,7 +44,7 @@ void	manage_quotes(void)
 	}
 	while ((*stock()) && (*stock())[i])
 	{
-		if ((*stock())[i] == '"' && !(q % 2))
+		if ((*stock())[i] == '"' && !(q % 2) && !(*bs()))
 			dq++;
 		if ((*stock())[i] == '\'' && !(dq % 2))
 			q++;
