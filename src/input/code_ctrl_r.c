46dfe40ca3c51417469abb5b495700e3597beaee
Adam TAGUIROV <ataguiro@e2r6p13.42.fr>
2017-01-24 19:22:41 +0100

    almost done

--- src/input/code_ctrl_r.c
+++ src/input/code_ctrl_r.c
@@ -6,15 +6,18 @@
 /*   By: ataguiro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/24 17:13:29 by ataguiro          #+#    #+#             */
-/*   Updated: 2017/01/24 19:08:04 by ataguiro         ###   ########.fr       */
+/*   Updated: 2017/01/24 19:22:31 by ataguiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "input.h"
 
-static void	display_missing()
+static void	display_missing(char *tmp)
 {
-	
+	default_mode();
+	tmp = &tmp[cor()->x];
+	ft_putstr(tmp);
+	move_left(ft_strlen(tmp));
 }
 
 void		code_ctrl_r(void)
@@ -23,8 +26,12 @@ void		code_ctrl_r(void)
 	char		*pattern;
 	t_list		*full_list;
 
+	if (!(*search_mode()))
+	{
+		*search_mode() = 1;
+		return ;
+	}
 	pattern = ft_strjoin(*command(), "*");
-	*search_mode() = 1;
 	full_list = *get_full_list();
 	while (full_list)
 	{
