6cde3c775cde1e6720b4712bf75b02cc48363d87
Fanny HUANG <fhuang@e3r9p20.42.fr>
2017-02-02 16:40:02 +0100

    Fix backslash + \n + quotes

--- src/expansion/quotes.c
+++ src/expansion/quotes.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 13:27:22 by fhuang            #+#    #+#             */
-/*   Updated: 2017/02/02 16:25:03 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/02/02 16:38:11 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,6 @@
 
 #define SQUOTE_OPEN (squote % 2) == 1
 #define DQUOTE_OPEN (dquote % 2) == 1
-
-static char		*remove_newlines(char *cmd)
-{
-	int		i;
-	int		j;
-
-	i = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '\n' && (i > 0 && cmd[i - 1] == '\\'))
-		{
-			j = ++i;
-			while (cmd[i])
-			{
-				cmd[i - 1] = cmd[i];
-				i++;
-			}
-			ft_strclr(cmd + i - 1);
-			i = j;
-		}
-		else
-			++i;
-	}
-	return (cmd);
-}
 
 static char		*strcpy_without_blank(char *src)
 {
@@ -68,11 +43,12 @@ char			*remove_quotes_and_backslash(char *cmd)
 	i = 0;
 	squote = 0;
 	dquote = 0;
-	cmd = remove_newlines(cmd);
 	while (cmd[i])
 	{
 		backslash = (i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN)) ? 1 : 0;
-		if (cmd[i] == '\'' && !(DQUOTE_OPEN) &&\
+		if (cmd[i] == '\n' && backslash == 1 && !(SQUOTE_OPEN) && !(DQUOTE_OPEN))
+			cmd[i] = 127;
+		else if (cmd[i] == '\'' && !(DQUOTE_OPEN) &&\
 			(backslash == 0))
 		{
 			cmd[i] = 127;
@@ -88,7 +64,7 @@ char			*remove_quotes_and_backslash(char *cmd)
 			cmd[i + 1] = 127;
 			i++;
 		}
-		else if (cmd[i] == '\\' && !(SQUOTE_OPEN) && cmd[i + 1] != '\'' && cmd[i + 1] != '\"')
+		else if (cmd[i] == '\\' && !(SQUOTE_OPEN) && cmd[i + 1] != '\'' && cmd[i + 1] != '\"' && cmd[i + 1] != '\n')
 			cmd[i] = 127;
 		else if (cmd[i] == '\'' && backslash == 1 && !(DQUOTE_OPEN))
 			cmd[i - 1] = 127;
