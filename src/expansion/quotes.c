a43fe1e07d015abdfd6c45fc98fbc7045b078eb3
Fanny HUANG <fhuang@e3r6p15.42.fr>
2017-02-03 17:57:49 +0100

    norme quotes file

--- src/expansion/quotes.c
+++ src/expansion/quotes.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 13:27:22 by fhuang            #+#    #+#             */
-/*   Updated: 2017/02/03 17:41:18 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/02/03 17:57:27 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@
 #define SQUOTE_OPEN (bf.squote % 2) == 1
 #define DQUOTE_OPEN (bf.dquote % 2) == 1
 
-static char		*remove_newlines(char *cmd, int i)
+static char		*remove_newlines(char *cmd, int *i)
 {
-	while (cmd[i + 1])
+	int		tmp;
+
+	tmp = *i;
+	while (cmd[tmp + 1])
 	{
-		cmd[i] = cmd[i + 1];
-		++i;
+		cmd[tmp] = cmd[tmp + 1];
+		++(tmp);
 	}
-	ft_strclr(cmd + i);
+	ft_strclr(cmd + tmp);
+	--(*i);
 	return (cmd);
 }
 
@@ -54,29 +58,19 @@ char			*remove_quotes_and_backslash(char *cmd)
 	while (cmd[++i])
 	{
 		if (cmd[i] == '\n' && i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN))
-		{
-			cmd = remove_newlines(cmd, i);
-			--i;
-		}
-		bf.bslash = (i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN)) ? 1 : 0;
+			cmd = remove_newlines(cmd, &i);
 		if (cmd[i] == '\\' && !(SQUOTE_OPEN) && cmd[i + 1] == '\\' && (i++))
 			cmd[i] = 127;
 		else if (!(SQUOTE_OPEN) && cmd[i] == '\\' &&\
 			cmd[i + 1] != '\'' && cmd[i + 1] != '\"' && cmd[i + 1] != '\n')
 			cmd[i] = 127;
-		else if (cmd[i] == '\'' && !(DQUOTE_OPEN) && bf.bslash == 0)
+		else if ((cmd[i] == '\'' && !(DQUOTE_OPEN)) ||\
+			(cmd[i] == '\"' && !(SQUOTE_OPEN)))
 		{
-			++bf.squote;
-			cmd[i] = 127;
+			bf.bslash = (i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN)) ? 1 : 0;
+			cmd[i - bf.bslash] = 127;
+			bf.bslash == 0 && cmd[i] == '\'' ? ++bf.squote : ++bf.dquote;
 		}
-		else if (cmd[i] == '\"' && bf.bslash == 0 && !(SQUOTE_OPEN))
-		{
-			++bf.dquote;
-			cmd[i] = 127;
-		}
-		else if (bf.bslash == 1 && ((cmd[i] == '\'' && !(DQUOTE_OPEN)) ||\
-			(cmd[i] == '\"' && !(SQUOTE_OPEN))))
-			cmd[i - 1] = 127;
 	}
 	return (strcpy_without_blank(cmd));
 }
