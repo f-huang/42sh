786ad1e79dd89d8f78df827f4ee87603c36086c4
Fanny HUANG <fhuang@e3r6p19.42.fr>
2017-02-05 13:58:09 +0100

    fix_quotes

--- src/expansion/quotes.c
+++ src/expansion/quotes.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 13:27:22 by fhuang            #+#    #+#             */
-/*   Updated: 2017/02/03 17:57:27 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/02/05 13:57:15 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,17 @@ char			*remove_quotes_and_backslash(char *cmd)
 	{
 		if (cmd[i] == '\n' && i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN))
 			cmd = remove_newlines(cmd, &i);
+		bf.bslash = (i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN)) ? 1 : 0;
 		if (cmd[i] == '\\' && !(SQUOTE_OPEN) && cmd[i + 1] == '\\' && (i++))
 			cmd[i] = 127;
 		else if (!(SQUOTE_OPEN) && cmd[i] == '\\' &&\
 			cmd[i + 1] != '\'' && cmd[i + 1] != '\"' && cmd[i + 1] != '\n')
 			cmd[i] = 127;
-		else if ((cmd[i] == '\'' && !(DQUOTE_OPEN)) ||\
-			(cmd[i] == '\"' && !(SQUOTE_OPEN)))
+		else if (((cmd[i] == '\'' && !(DQUOTE_OPEN)) ||\
+			(cmd[i] == '\"' && !(SQUOTE_OPEN))))
 		{
-			bf.bslash = (i > 0 && cmd[i - 1] == '\\' && !(SQUOTE_OPEN)) ? 1 : 0;
+			bf.bslash == 0 && cmd[i] == '\'' ? (bf.squote)++ : (bf.dquote)++;
 			cmd[i - bf.bslash] = 127;
-			bf.bslash == 0 && cmd[i] == '\'' ? ++bf.squote : ++bf.dquote;
 		}
 	}
 	return (strcpy_without_blank(cmd));
