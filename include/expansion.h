b596492cc6ba825f54100056afac1fc5fe63dec2
Fanny HUANG <fhuang@e3r6p19.42.fr>
2017-02-05 18:22:35 +0100

    Cleaned useless things

--- include/expansion.h
+++ include/expansion.h
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 18:23:46 by fhuang            #+#    #+#             */
-/*   Updated: 2017/02/05 17:17:59 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/02/05 18:19:59 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,5 @@ char					*exclamation_mark(char *cmd);
 
 int						alias_substitution(t_variable *lst_alias, char **line);
 t_list					*glob_substitution(t_list **lst_tab);
-
-int						subshell_substitution(char **comand);
 
 #endif
